a1b1be9e4d6e48f20e726b1d83703eae8288c772
Anupong Phonjanthuk <128229081+conankxng@users.noreply.github.com>
2026-03-24 00:58:52 +0000

    ss

--- index4.py
+++ index4.py
@@ -4,8 +4,10 @@ os.system('cls')
 def main():
         product = int(input('กรุณากรอกจำนวนสินค้าที่ต้องซื้อ :'))
         n = price_type(product)
+
         print(f'ราคาต่อชิ้นที่ต้องจ่าย {n:,.2f} บาท')
         print(f'ราคารวมทั้งหมด {n*product:,.2f} บาท')
+
 def price_type(product):
     if product>=50:
         n = 80
